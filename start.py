53a232d8dbe2e9ba36e9475cc8625c52db62d8f4
Oliver Frischknech <oliver.frischi@gmail.com>
2011-03-07 07:51:48 +0100

    First commit

---
+++ start.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+import re
+import time
+
+def findThisProcess( process_name ):
+    ps     = subprocess.Popen("ps -eaf | grep "+process_name, shell=True, stdout=subprocess.PIPE)
+    output = ps.stdout.read()
+    ps.stdout.close()
+    ps.wait()
+
+    return output
+
+# This is the function you can use  
+def isThisRunning( process_name ):
+    output = findThisProcess( process_name )
+    if re.search('path/of/process'+process_name, output) is None:
+        return False
+    else:
+        return True
+
+while True:
+    time.sleep(5)
+    if isThisRunning('gossip-remover') == False:
+        print("Not running")
+        print("start gossip-remover")
+        os.system('python /home/oliver/gossip-remover/gossip-remover.py')
+    else:
+        print("Running!")
